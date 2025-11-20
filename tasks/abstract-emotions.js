fcbdea575b6adaf6474f9658237b076cb22798a2
sabrichanguiti <changuiti.sabri@gmail.com>
2025-11-20 13:24:12 +0100

    temporary

--- tasks/abstract-emotions.js
+++ tasks/abstract-emotions.js
@@ -5,6 +5,8 @@ background("white")
 
 let middleHole = 200
 let donut = 400
+let amount = 5000
+let size = 30
 
 let centerX = windowWidth/2
 let centerY = windowHeight/2
@@ -12,4 +14,6 @@ let centerY = windowHeight/2
 fill("orange")
 circle(centerX, centerY, 200)
 fill("black")
-circle(centerX, centerY, 30)
+circle(centerX, centerY, size)
+
+for (let i = 0; i < amount; i++)
