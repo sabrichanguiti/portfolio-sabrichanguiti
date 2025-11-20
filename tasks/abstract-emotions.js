a169c9a34c2ab034308ddbdc6a8d0049b681c163
sabrichanguiti <changuiti.sabri@gmail.com>
2025-11-20 12:38:54 +0100

    taak begonnen

---
+++ tasks/abstract-emotions.js
@@ -0,0 +1,13 @@
+// Sabri CHANGUITI - "Verraden/Vertrouwd"
+
+createCanvas(windowWidth, windowHeight)
+background("white")
+
+let middleHole = 200
+
+fill("orange")
+circle(windowWidth/2, windowHeight/2, 200)
+fill("black")
+circle(windowWidth/2, windowHeight/2, 30)
+
+
