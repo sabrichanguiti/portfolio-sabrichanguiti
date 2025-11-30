a9af74a417dd7ab1c0153f2b671fed692aab590e
sabrichanguiti <changuiti.sabri@gmail.com>
2025-11-30 21:53:16 +0100

    restarted

---
+++ sketches/module-06/array.js
@@ -0,0 +1,10 @@
+function setup() {
+    let names = [];
+    names[0] = "Alice";
+    names[1] = "Bob"
+    names[2] = "Charlie"
+    names[3] = "Diana"
+    names[4] = "Ethan"
+
+    output(names[random(0, 4)])
+}
